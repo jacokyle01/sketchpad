93b704ea193ea90051aa6334ba8ab8a99470cbec
Kyle Jacobson <jacokyle01@gmail.com>
2023-06-17 15:48:45 -0500

    Add slider for selecting grid size

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const NUM_BUTTONS = 10;
 const container = document.querySelector(".container");
+const output = document.querySelector(".output");
+const input = document.querySelector("input");
 
-//generate rows
-for (let row = 0; row < NUM_BUTTONS; row++) {
-  const row = document.createElement("div");
-  row.id = "row";
-  container.appendChild(row);
+window.onload = function() {
+    initializeGrid(NUM_BUTTONS);
+    output.innerHTML = NUM_BUTTONS;
+}
 
-  //generate buttons
-  for (let i = 0; i < NUM_BUTTONS; i++) {
-    const button = document.createElement("button");
-    row.appendChild(button);
+
+function initializeGrid(size) {
+  for (let row = 0; row < size; row++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    container.appendChild(row);
+    for (let i = 0; i < size; i++) {
+      const square = document.createElement("div");
+      square.className = "square";
+      row.appendChild(square);
+    }
   }
 }
+
+function resetContainer() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+input.oninput = function () {
+  output.innerHTML = this.value;
+  resetContainer();
+  initializeGrid(this.value);
+};
