ed8cd602244edc98a8021611e3915bbce0d6a31e
Kyle Jacobson <jacokyle01@gmail.com>
2023-06-18 18:47:19 -0500

    Add palette for color selecting

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ const NUM_BUTTONS = 10;
 const container = document.querySelector(".container");
 const output = document.querySelector(".output");
 const input = document.querySelector("input");
+let selectedColor = null;
 
 window.onload = function() {
     initializeGrid(NUM_BUTTONS);
     output.innerHTML = NUM_BUTTONS;
+    initializePalette();
+    //default choice is black 
+    const black = document.querySelector("#black");
+    updatePalette(black);
 }
 
+//called once on load 
+function initializePalette() {
+  const colors = document.querySelectorAll('.color');
+  colors.forEach(color => {
+    color.style.backgroundColor = color.id;
+    color.addEventListener("click", function() {
+      updatePalette(color);
+    });
+  })
+}
 
+//called when slider is updated
 function initializeGrid(size) {
   for (let row = 0; row < size; row++) {
     const row = document.createElement("div");
@@ -18,14 +34,22 @@ function initializeGrid(size) {
       const square = document.createElement("div");
       square.className = "square";
       square.addEventListener('mouseleave', function() {
-        square.style.backgroundColor = "black";
+        square.style.backgroundColor = selectedColor.id;
       });
       row.appendChild(square);
     }
   }
-
-  //and add event listeners
 }
+
+//called when palette is updated
+function updatePalette(newColor) {
+  newColor.style.border = "3px solid red";
+  if (selectedColor != null) {
+    selectedColor.style.border = "none";
+  }
+  selectedColor = newColor;
+}
+
 
 function resetContainer() {
     while (container.firstChild) {
