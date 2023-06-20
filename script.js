5b5d8eb869d0449633a3464d1d4af52534b69f4b
Kyle Jacobson <jacokyle01@gmail.com>
2023-06-19 23:39:57 -0500

    Fix mouse dragging for line creation

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const output = document.querySelector(".output");
 const input = document.querySelector("input");
 let selectedColor = null;
 
-window.onload = function() {
-    initializeGrid(NUM_BUTTONS);
-    output.innerHTML = NUM_BUTTONS;
-    initializePalette();
-    //default choice is black 
-    const black = document.querySelector("#black");
-    updatePalette(black);
-}
+window.onload = function () {
+  initializeGrid(NUM_BUTTONS);
+  output.innerHTML = NUM_BUTTONS;
+  initializePalette();
+  //default choice is black
+  const black = document.querySelector("#black");
+  updatePalette(black);
+};
 
-//called once on load 
+//called once on load
 function initializePalette() {
-  const colors = document.querySelectorAll('.color');
-  colors.forEach(color => {
+  const colors = document.querySelectorAll(".color");
+  colors.forEach((color) => {
     color.style.backgroundColor = color.id;
-    color.addEventListener("click", function() {
+    color.addEventListener("click", function () {
       updatePalette(color);
     });
-  })
+  });
 }
 
 //called when slider is updated
@@ -29,37 +29,44 @@ function initializeGrid(size) {
   for (let row = 0; row < size; row++) {
     const row = document.createElement("div");
     row.className = "row";
+    //row.setAttribute('draggable', 'false');
     container.appendChild(row);
     for (let i = 0; i < size; i++) {
       const square = document.createElement("div");
-      square.className = "square";
-      square.addEventListener('mouseleave', function() {
-        square.style.backgroundColor = selectedColor.id;
-      });
+      square.classList.add("square");
+
+      square.addEventListener('mouseover', changeColor);
+      square.addEventListener('mousedown', changeColor);
+
+      
       row.appendChild(square);
     }
+  }
+}
+
+function changeColor(e) {
+  //if (e.type === 'mouseover' && !mouseDown) return
+  if (e.buttons === 1) {
+    e.target.style.backgroundColor = selectedColor.id;
   }
 }
 
 //called when palette is updated
 function updatePalette(newColor) {
   newColor.style.border = "3px solid red";
-  if (selectedColor != null) {
+  if (selectedColor != null && selectedColor != newColor) {
     selectedColor.style.border = "none";
   }
   selectedColor = newColor;
 }
 
-
 function resetContainer() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 }
 
-console.log()
-
-
+console.log();
 
 input.oninput = function () {
   output.innerHTML = this.value;
