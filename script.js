8b4d8521e04a582a2d380a1aa7cfab066d6d44ef
Kyle Jacobson <jacokyle01@gmail.com>
2023-06-22 17:29:58 -0500

    Reduce coupling by creating `paint()` method `paint()` method should call `querySquaresInRange()` to generate an array of squares, then call `changeColor()` to to color squares depending on `selectedColor`. This allows the program to more freely handle variations in `brushSize` and `mode`.

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const NUM_BUTTONS = 10;
+const DEFAULT_BRUSH_SIZE = 2;
+const DEFAULT_MODE = 'color';
+
 const container = document.querySelector(".container");
 const output = document.querySelector(".output");
 const input = document.querySelector("input");
 let selectedColor = null;
+let brushSize = null;
+let mode = null;
 
 window.onload = function () {
   initializeGrid(NUM_BUTTONS);
   output.innerHTML = NUM_BUTTONS;
   initializePalette();
+  brushSize = DEFAULT_BRUSH_SIZE;
+  mode = DEFAULT_MODE;
   //default choice is black
   const black = document.querySelector("#black");
   updatePalette(black);
@@ -29,9 +36,12 @@ function initializeGrid(size) {
   for (let i = 0; i < size * size; ++i) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.addEventListener("mousemove", changeColor);
-    square.addEventListener("mousedown", changeColor);
+    square.addEventListener("mousemove", paint);
+    square.addEventListener("mousedown", paint);
 
+
+    square.setAttribute("data-row", Math.floor(i / size));
+    square.setAttribute("data-col", i % size);
     square.style.width = 100 / size + "%";
     square.style.height = 100 / size + "%";
 
@@ -39,11 +49,42 @@ function initializeGrid(size) {
   }
 }
 
-function changeColor(e) {
-  if (e.buttons === 1) {
-    e.target.style.backgroundColor = selectedColor.id;
+function querySquaresInRange(sourceSquare, range) {
+  const sourceRow = +sourceSquare.getAttribute("data-row");
+  const sourceCol = +sourceSquare.getAttribute("data-col");
+  console.log("source " + "(" + sourceRow + ", " + sourceCol + ")");
+  let squaresInRange = [];
+
+  for (let targetRow = sourceRow - range; targetRow <= sourceRow + range; targetRow++) {
+    for (let targetCol = sourceCol - range; targetCol <= sourceCol + range; targetCol++) {
+      console.log("searching " + "(" + targetRow + " , " + targetCol + ")");
+      const selector = `[data-row="${targetRow}"][data-col="${targetCol}"]`;
+      const targetSquare = document.querySelector(selector);
+      if (targetSquare != null) {
+        squaresInRange.push(targetSquare);
+      }
+    }
+  }
+
+  return squaresInRange;
+}
+
+function paint(e) {
+  if (e.buttons != 1) return;
+  const targetSquare = e.target;
+
+  const affectedSquares = querySquaresInRange(targetSquare, brushSize);
+  affectedSquares.forEach(square => changeColor(square));
+}
+
+function changeColor(square) {
+  switch(mode) {
+    case 'color':
+      square.style.backgroundColor = selectedColor.id;
+      break;
   }
 }
+
 
 //called when palette is updated
 function updatePalette(newColor) {
